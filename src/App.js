ea517939103ad0d4d8a36b596f4815deb84b52fb
Clement Bourgoin <cb@nokto.net>
2017-06-28 17:18:12 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1 className="App-title">Clément Bourgoin</h1>
+          <ul className="App-menu">
+            <li>Compétences</li>
+            <li>Expérience</li>
+            <li>Projets</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <p className="App-intro">
+          Work in progress
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
