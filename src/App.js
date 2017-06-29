9437b24bfc7be4ac499c20a8fa570d599299c153
Clement Bourgoin <cb@nokto.net>
2017-06-29 11:41:24 +0200

    Add a toggable Skills page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import SkillsPage from './pages/Skills';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isSkillsPageVisible: false
+    }
+  }
+
+  displaySkillsPage() {
+    this.setState({
+      isSkillsPageVisible: true
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1 className="App-title">Clément Bourgoin</h1>
           <ul className="App-menu">
-            <li>Compétences</li>
+            <li onClick={this.displaySkillsPage.bind(this)}>Compétences</li>
             <li>Expérience</li>
             <li>Projets</li>
             <li>Contact</li>
           </ul>
         </div>
-        <p className="App-intro">
-          Work in progress
-        </p>
+        {this.state.isSkillsPageVisible && <SkillsPage />}
       </div>
     );
   }
---
+++ src/pages/Skills.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SkillsPage extends Component {
+  render() {
+    return (
+      <div className="skills-page page">
+        <h1>Compétences</h1>
+      </div>
+    );
+  }
+}
+
+export default SkillsPage;
