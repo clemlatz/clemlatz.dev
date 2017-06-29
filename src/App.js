e964e600fd3d57e01229194adfe922b00195aaab
Clement Bourgoin <cb@nokto.net>
2017-06-29 12:15:32 +0200

    Add toggable Home page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HomePage from './pages/Home';
 import SkillsPage from './pages/Skills';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      isSkillsPageVisible: false
+      isHomePageVisible: true,
+      isSkillsPageVisible: false,
+      pagesVisibility: {
+        home: true,
+        skills: false
+      },
+      displayedPage: 'home',
     }
   }
 
-  displaySkillsPage() {
-    this.setState({
-      isSkillsPageVisible: true
-    });
+  displayPage(page) {
+    this.hideDisplayedPage();
+    const pagesVisibility = this.state.pagesVisibility;
+    pagesVisibility[page] = true;
+    this.setState({ pagesVisibility: pagesVisibility, displayedPage: page });
+  }
+
+  hideDisplayedPage() {
+    const displayedPage = this.state.displayedPage;
+    const pagesVisibility = this.state.pagesVisibility;
+    pagesVisibility[displayedPage] = false;
+    this.setState({ pagesVisibility: pagesVisibility });
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1 className="App-title">Clément Bourgoin</h1>
+          <h1 className="App-title" onClick={() => this.displayPage('home')}>Clément Bourgoin</h1>
           <ul className="App-menu">
-            <li onClick={this.displaySkillsPage.bind(this)}>Compétences</li>
+            <li onClick={() => this.displayPage('skills')}>Compétences</li>
             <li>Expérience</li>
             <li>Projets</li>
             <li>Contact</li>
           </ul>
         </div>
-        {this.state.isSkillsPageVisible && <SkillsPage />}
+        {this.state.pagesVisibility['home'] && <HomePage />}
+        {this.state.pagesVisibility['skills'] && <SkillsPage />}
       </div>
     );
   }
