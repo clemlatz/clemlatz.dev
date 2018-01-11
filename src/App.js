249af0c9515d7a40693fa5f184cc0a12d79b4d0c
Clement Bourgoin <cb@nokto.net>
2018-01-11 13:48:25 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import './App.css';
 
@@ -12,20 +12,23 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <header>
-            <h1 className="site-title">Clément Bourgoin</h1>
+            <h1 className="site-title">
+              <Link to="/">Clément Bourgoin</Link>
+            </h1>
             <nav className="App-navigation">
               <ul>
                 <li>
-                  <a href="/experiences">Parcours</a>
+                  <Link to="/experiences">Parcours</Link>
                 </li>
                 <li>
-                  <a href="/contact">Contact</a>
+                  <Link to="/contact">Contact</Link>
                 </li>
               </ul>
             </nav>
           </header>
-          <ExperienceList/>
-          <Contact/>
+          <Route path="/" exact render={() => <div>Home</div>} />
+          <Route path="/experiences" component={ExperienceList} />
+          <Route path="/contact" component={Contact} />
         </div>
       </BrowserRouter>
     );
