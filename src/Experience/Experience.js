f7f6723962e44bf52f03efc81cd1708592d7d5ba
Clement Bourgoin <clement@actialuna.com>
2019-11-29 10:16:46 +0100

    Fix missing noopener noreferrer attributes in Experience component

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -4,17 +4,23 @@ import { Link } from 'react-router-dom';
 
 import './Experience.css';
 
-export default function(props) {
-
+export default function Experience(props) {
   let company = null;
   if (props.company) {
     company = `@ ${props.company}`;
   }
   if (props.companyUrl) {
-    company = <span>@ <a href={props.companyUrl} target="_blank">{props.company}</a></span>
+    company = (
+      <span>
+        @{' '}
+        <a href={props.companyUrl} target="_blank" rel="noopener noreferrer">
+          {props.company}
+        </a>
+      </span>
+    );
   }
 
-  let endYear = ' - Aujourd\'hui';
+  let endYear = " - Aujourd'hui";
   if (props.endYear) {
     endYear = ` - ${props.endYear}`;
     if (props.startYear === props.endYear) {
@@ -24,18 +30,22 @@ export default function(props) {
 
   let tags = null;
   if (props.tags) {
-    tags = props.tags.map(tag => <span key={tag} className="tag">#{tag}</span>);
+    tags = props.tags.map(tag => (
+      <span key={tag} className="tag">
+        #{tag}
+      </span>
+    ));
     tags = <div className="tags">{tags}</div>;
   }
 
   return (
     <div className="Experience">
       <Link key={props.slug} to={`/experiences/${props.slug}`}>
-        <h2 className="title">
-          {props.jobTitle}
-        </h2>
+        <h2 className="title">{props.jobTitle}</h2>
       </Link>
-      <span className="year">{props.startYear} {endYear} {company} </span>
+      <span className="year">
+        {props.startYear} {endYear} {company}{' '}
+      </span>
       {tags}
     </div>
   );
