42eebf8d4160c48b608cc450ad09318bb27657a8
Clement Bourgoin <c@iwzr.fr>
2020-01-23 09:53:20 +0100

    Clean old code from App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 import './App.css';
@@ -7,56 +7,37 @@ import ExperienceList from './ExperienceList/ExperienceList';
 import ExperiencePage from './ExperiencePage/ExperiencePage';
 import Links from './Links/Links';
 
-class App extends Component {
-  _fetchExperiences = async () => {
-    const response = await fetch('/data/experiences.json');
-    const experiences = await response.json();
+// Match locales with regular expression containing each locale separated by `|`
+const base = '/:locale(en|fr)?';
 
-    this.props.onExperiencesLoaded(experiences.experiences);
-  };
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <header>
-            <h1 className="site-title">
-              <NavLink to="/">Clément Bourgoin</NavLink>
-            </h1>
-            <nav className="App-navigation">
-              <ul>
-                <li>
-                  <NavLink to="/experiences">Parcours</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/links">Liens</NavLink>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <Route
-            path="/"
-            exact
-            render={() => <div>Développeur front-end à Paris</div>}
-          />
-          <Route
-            path="/experiences"
-            exact
-            render={() => (
-              <ExperienceList fetchExperiences={this._fetchExperiences} />
-            )}
-          />
-          <Route
-            path="/experiences/:slug"
-            render={() => (
-              <ExperiencePage fetchExperiences={this._fetchExperiences} />
-            )}
-          />
-          <Route path="/Links" component={Links} />
-        </div>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <h1 className="site-title">
+            <NavLink to={base}>Clément Bourgoin</NavLink>
+          </h1>
+          <nav className="App-navigation">
+            <ul>
+              <li>
+                <NavLink to={`${base}/experiences`}>Parcours</NavLink>
+              </li>
+              <li>
+                <NavLink to="/links">Liens</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route
+          path="/"
+          exact
+          render={() => <div>Développeur front-end à Paris</div>}
+        />
+        <Route path="/experiences" exact component={ExperienceList} />
+        <Route path="/experiences/:slug" component={ExperiencePage} />
+        <Route path="/Links" component={Links} />
+      </div>
+    </BrowserRouter>
+  );
 }
-
-export default App;
