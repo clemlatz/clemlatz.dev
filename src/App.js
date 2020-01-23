e282bc05e1241e7807805d2e8e4d6ae5841525df
Clement Bourgoin <c@iwzr.fr>
2020-01-23 13:08:10 +0100

    Internationalize header and home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
+
+import I18n from './I18n';
 
 import ExperienceList from './ExperienceList/ExperienceList';
 import ExperiencePage from './ExperiencePage/ExperiencePage';
 import Links from './Links/Links';
+import Header from './Header/Header';
 
 // Match locales with regular expression containing each locale separated by `|`
 const base = '/:locale(en|fr)?';
@@ -14,29 +17,21 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <header>
-          <h1 className="site-title">
-            <NavLink to={base}>Clément Bourgoin</NavLink>
-          </h1>
-          <nav className="App-navigation">
-            <ul>
-              <li>
-                <NavLink to={`${base}/experiences`}>Parcours</NavLink>
-              </li>
-              <li>
-                <NavLink to="/links">Liens</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        {/* Redirect root url to english by default */}
+        <Route path="/" exact render={() => <Redirect to="/en/" />} />
+
+        {/* Add Header as a route to inject translations */}
+        <Route path={base} component={Header} />
+
+        {/* Routes */}
         <Route
-          path="/"
+          path={base}
           exact
-          render={() => <div>Développeur front-end à Paris</div>}
+          render={() => <I18n t="Front-end web developer based in Paris" />}
         />
-        <Route path="/experiences" exact component={ExperienceList} />
+        <Route path={`${base}/experiences`} exact component={ExperienceList} />
         <Route path="/experiences/:slug" component={ExperiencePage} />
-        <Route path="/Links" component={Links} />
+        <Route path={`${base}/links`} component={Links} />
       </div>
     </BrowserRouter>
   );
---
+++ src/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-i18n';
+import I18n from '../I18n';
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="site-title">
+        <NavLink to="/">Clément Bourgoin</NavLink>
+      </h1>
+      <nav className="App-navigation">
+        <ul>
+          <li>
+            <NavLink to="/experiences">
+              <I18n t="Career" />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/links">
+              <I18n t="Links" />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
