17cbcf2b1855a2dbc48399169533e8a0f41e43b4
Clement Bourgoin <c@iwzr.fr>
2020-01-24 14:44:45 +0100

    Remove useless meta tags

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,12 +9,10 @@ export default function Home({ location }) {
   return (
     <div>
       <Helmet>
-        <meta charSet="utf-8" />
         <title>
           Clément Bourgoin ·{' '}
           {`${I18n.getTranslation(location, 'front-end web developer')}`}
         </title>
-        <link rel="canonical" href="https://iwazaru.dev/" />
       </Helmet>
       <p>
         <NavLink to="/en/">en</NavLink> · <NavLink to="/fr/">fr</NavLink>
