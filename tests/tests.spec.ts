1e3e696ed7d1337567e77a1522272e812af39557
Clément Latzarus <c@ltzr.io>
2024-11-22 09:03:12 +0100

    feat: add Projects page

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -6,3 +6,11 @@ test('displays home page', async ({ page }) => {
   await expect(page).toHaveTitle('Clément Latzarus · développeur web');
   await expect(page.getByRole('heading', { name: 'Clément Latzarus' })).toBeVisible();
 });
+
+test('displays projects page', async ({ page }) => {
+  await page.goto('/projects');
+
+  await expect(page).toHaveTitle('Projets · Clément Latzarus · développeur web');
+  await expect(page.getByRole('heading', { name: 'Projets' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Biblys'})).toBeVisible();
+});
