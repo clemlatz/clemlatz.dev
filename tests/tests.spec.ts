526ab56ea3acf05b0d0777361fa78430e109f742
Clément Latzarus <c@ltzr.io>
2024-11-23 18:08:02 +0100

    feat: remove links page

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -11,7 +11,6 @@ test('displays projects page', async ({page}) => {
   await page.goto('/projects');
 
   await expect(page).toHaveTitle('Projets · Clément Latzarus · développeur web');
-  await expect(page.getByRole('heading', {name: 'Projets'})).toBeVisible();
   await expect(page.getByRole('link', {name: 'Biblys'})).toBeVisible();
 });
 
