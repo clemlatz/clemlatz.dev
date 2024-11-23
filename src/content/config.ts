6a5e6cdc66eec21f01e69330678fea84a95cc42a
Clément Latzarus <c@ltzr.io>
2024-11-23 13:29:58 +0100

    feat: add blog collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,21 @@ const projetsCollection = defineCollection({
   }),
 });
 
+const blogCollection = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    language: z.enum(['en', 'fr']),
+  }),
+});
+
 export const collections = {
   'projects': projetsCollection,
+  'blog': blogCollection,
 };
