11ad5c8fd341cd7d4366f7527f9ab7f35027c727
Clément Latzarus <c@ltzr.io>
2024-11-23 17:24:36 +0100

    build: add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   site: 'https://clemlatz.dev',
 });
