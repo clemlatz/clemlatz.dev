d00067622234156e2b76ca4c26fbefff9621855a
Clément Latzarus <c@ltzr.io>
2025-07-30 18:22:43 +0200

    feat: add umami tracking code

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
-/// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/types.d.ts" /
+
+export {};
+
+declare global {
+    interface Window {
+        umami: {
+            track: (eventName: string, eventData?: Record<string, any>) => void;
+        }; // Replace `any` with a more specific type if known
+    }
+}
