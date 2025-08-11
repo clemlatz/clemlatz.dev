19381dec57807fa9b1795c2a4a8dd99a9162083d
Clément Latzarus <c@ltzr.io>
2025-08-11 18:24:00 +0200

    fix: hide draft posts in rss feed

--- src/pages/blog.rss.xml.ts
+++ src/pages/blog.rss.xml.ts
@@ -5,15 +5,14 @@ type BlogPost = CollectionEntry<'blog'>;
 
 // noinspection JSUnusedGlobalSymbols
 export async function GET(context: { site: any; }) {
-  const posts = await getCollection('blog');
-  const sortedPosts = posts.sort(
-    (a: BlogPost, b: BlogPost) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  );
+  const posts = (await getCollection('blog'))
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .filter(post => !post.data.draft);
   return rss({
     title: "Le blog de Clément Latzarus",
     description: "",
     site: context.site,
-    items: sortedPosts.map((post: BlogPost) => ({
+    items: posts.map((post: BlogPost) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
